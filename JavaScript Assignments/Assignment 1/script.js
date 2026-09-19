fc49b8851af5d6b0fd72c842266a38df7a4419ba
https://github.com/MAbdullah124/webdevassignments.git <abdullah123@gmail.com>
2026-09-19 09:16:23 -0700

    added JavaScript assignments folder

---
+++ JavaScript Assignments/Assignment 1/script.js
@@ -0,0 +1,35 @@
+// Step 1: Hello World
+
+alert("Hello World");
+
+console.log("Hello World");
+
+
+// Step 2: Create Variables
+
+var studentName = "Abdullah";
+
+let courseName = "Web Development";
+
+const instituteName = "SMIT Peshawar";
+
+
+// Step 3: Display Variables in Console
+
+console.log(studentName);
+
+console.log(courseName);
+
+console.log(instituteName);
+
+
+// Step 4: Display Variable on Webpage
+
+document.write(studentName);
+
+
+// Step 5: Change the value of let variable
+
+courseName = "Generative AI";
+
+console.log(courseName);
